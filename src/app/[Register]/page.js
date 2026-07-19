e22e0d4bdb69f79e1c2b6bb128ca22da0a7c3365
Ayush Kumar <helloayush135@gmail.com>
2026-07-19 17:50:25 +0530

    First Commit

---
+++ src/app/[Register]/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className="">
+      <div className="mt-10 font-bold text-3xl text-center font-mono">
+        WELCOME TO BHARAT SINGS!
+      </div>
+    </div>
+  );
+}
+
+export default Register;
