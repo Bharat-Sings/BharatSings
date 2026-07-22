e79564890d5a6348d6de20b4de3664ab867936b4
Ayush Kumar <helloayush135@gmail.com>
2026-07-22 22:11:19 +0530

    Started Writing songs controller

---
+++ src/app/backend/controllers/song.controller.js
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const createSong = asyncHandler(async (req, res) => {
+    
+});
