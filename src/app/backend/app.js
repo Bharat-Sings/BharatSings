a60c1a227703c5892423f420e8f88bb582b64a13
Ayush Kumar <helloayush135@gmail.com>
2026-07-22 22:46:10 +0530

    Updated app.js

--- src/app/backend/app.js
+++ src/app/backend/app.js
@@ -4,6 +4,8 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./routers/user.router.js";
+import { songRouter } from "./routers/song.router.js";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,8 @@ app.use(cors({
     origin: process.env.FRONTEND_URI || "https://localhost:5000",
     credentials: true
 }));
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/songs", songRouter);
 
 const PORT = process.env.PORT || 3000;
 
