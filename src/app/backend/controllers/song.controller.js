a96fb2880474145214aca7536d0594757aef7fc8
Ayush Kumar <helloayush135@gmail.com>
2026-07-22 22:25:55 +0530

    Updated song controller

--- src/app/backend/controllers/song.controller.js
+++ src/app/backend/controllers/song.controller.js
@@ -1,8 +1,35 @@
-import { PrismaClient } from "@prisma/client/extension";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const createSong = asyncHandler(async (req, res) => {
-    
+    let { title, description, genre } = req.body;
+
+    if (
+        [title, description, genre].some((field) => !field || field?.trim() === "")
+    ) {
+        throw new ApiError(401, "All fields are required");
+    }
+
+    let song = await prisma.song.create({
+        data: {
+            title,
+            description,
+            genre
+        }
+    });
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                createdSong: song
+            },
+            "Song created successfully")
+    );
 });
