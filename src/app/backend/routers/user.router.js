a576ecdcb03fcea8aff33d830f827b3d44157b22
Purab <1609purab@gmail.com>
2026-07-23 15:06:36 +0530

    feat(auth): Register logion features

--- src/app/backend/routers/user.router.js
+++ src/app/backend/routers/user.router.js
@@ -1,5 +1,43 @@
 import { Router } from "express";
+import {
+
+    register,
+    login,
+    refreshToken,
+    logout,
+
+} from "../controllers/user.controller.js";
+
+import {
+
+    registerValidation,
+    loginValidation,
+
+} from "../validators/auth.validator.js";
+
+
 
 const router = Router();
 
+router.post(
+    "/register",
+    registerValidation,
+    register
+);
+
+router.post(
+    "/login",
+    loginValidation,
+    login
+);
+
+router.post(
+    "/refresh",
+    refreshToken
+);
+
+router.post(
+    "/logout",
+    logout
+);
 export { router as userRouter }
